03eaee54c8d48e1ab815e15f73f841c2f170ca5e
Momchil Petrov <momchil.petrov@axelspringer.com>
2026-02-06 10:36:05 +0100

    initial commit

---
+++ src/main/java/berlin/tu/cyclinginfrastructurebackend/CyclingInfrastructureBackendApplication.java
@@ -0,0 +1,13 @@
+package berlin.tu.cyclinginfrastructurebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyclingInfrastructureBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyclingInfrastructureBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/berlin/tu/cyclinginfrastructurebackend/CyclingInfrastructureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package berlin.tu.cyclinginfrastructurebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyclingInfrastructureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
